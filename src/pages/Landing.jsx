76a02515a5796db355760c8ab8d1d63e539bbcbd
erentrk01 <erentarak1907@gmail.com>
2022-09-02 11:15:09 +0300

    bootsrap and landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import {init} from "../utils/initAnimeLayer";
-import BtnStylish from "../components/btnStylish";
+import style from '../Styling/landing.css';
+import Button from 'react-bootstrap/Button';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Landing = () => {
 	useEffect(() => {
@@ -8,16 +10,17 @@ const Landing = () => {
 	},[]);
 	return(
 		<div>
-			<div className="hero">
+			
+			<div className="hero py-3 px-2">
 				<h1 className="title">Melodify</h1>
 				<p className="text">
 					A social radio chat app for music lovers.
 
 				</p>
-				
-				
-				<BtnStylish></BtnStylish>
-
+				<div className="d-flex justify-content-space-between  p-1">
+					<button className="btn btn-outline-danger">English</button>
+					<button className="btn btn-outline-danger ms-2">Turkish</button>
+				</div>
 				
 		</div>
 		</div>
