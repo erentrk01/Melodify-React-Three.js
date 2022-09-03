cac438ec74621f60c613e840a76e19b2414b7c64
erentrk01 <erentarak1907@gmail.com>
2022-09-03 15:11:22 +0300

    filter stations

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-
-
+import Radio from "./components/Radio";
 import Landing from "./pages/Landing";
 
 function App() {
@@ -11,6 +10,7 @@ function App() {
     <div className="App">
 	  <Routes>
 		<Route path="/" element={<Landing/>}/>
+		<Route path="/radio" element={<Radio/>}/>
 		</Routes>
     </div>
   );
---
+++ src/services/filter.js
@@ -0,0 +1,21 @@
+export const filters = [
+	'all',
+	'classical',
+	'country',
+	'dance',
+	'disco',
+	'electronic',
+	'folk',
+	'funk',
+	'hiphop',
+	'jazz',
+	'latin',
+	'metal',
+	'pop',
+	'punk',
+	'rnb',
+	'rock',
+	'soul',
+	'retro'
+
+];
